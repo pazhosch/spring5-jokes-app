df770ec106ffbb7e55091b229e5bf266bde0afb1
pazhosch <d.m.baranov@gmail.com>
2019-05-06 06:51:05 +0300

    fix: controller (following the tutorial)

--- src/main/java/guru/springframework/joke/controllers/JokeController.java
+++ src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -2,6 +2,7 @@ package guru.springframework.joke.controllers;
 
 import guru.springframework.joke.services.JokeService;
 import guru.springframework.joke.services.JokeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JokeController {
 
-    @RequestMapping("/")
-    public String getJoke(Model model) {
-        JokeService service = new JokeServiceImpl();
-        model.addAttribute("joke", service.getRandomQuote());
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getRandomQuote());
         return "chucknorris";
     }
 }
