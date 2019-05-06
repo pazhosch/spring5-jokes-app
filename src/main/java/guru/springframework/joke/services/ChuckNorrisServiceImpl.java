39b45bdf4bc6ea31946fd6f1a5c1e0ac4ddd455b
pazhosch <d.m.baranov@gmail.com>
2019-05-06 06:34:35 +0300

    warnings removed

--- src/main/java/guru/springframework/joke/services/ChuckNorrisServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/ChuckNorrisServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ChuckNorrisServiceImpl implements ChuckNorrisService {
-    ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
+    private ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
 
     @Override
     public String getRandomQuote() {
